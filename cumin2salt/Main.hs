1d8a8a862df10301421cf5537799a84269359ed9
Fabian Zaiser <fabian.zaiser@googlemail.com>
2015-03-02 17:41:30 +0100

    Write translated program to a file instead of stdout.

--- cumin2salt/Main.hs
+++ cumin2salt/Main.hs
@@ -8,9 +8,11 @@ import           Language.CuMin.Prelude           (preludeModule)
 import           Language.CuminToSalt.Translation (cuminToSalt, filterPrelude)
 import qualified Language.SaLT.Pretty             as SP
 import           Options.Applicative
+import qualified Text.PrettyPrint.ANSI.Leijen     as PP
 
 data Options = Options
-  { inputFile :: String
+  { inputFile   :: String
+  , outputFile  :: String
   , withPrelude :: Bool
   }
 
@@ -25,17 +27,21 @@ main = execParser opts >>= act
 optionParser :: Parser Options
 optionParser = Options
   <$> argument str (metavar "INPUT" <> help "the CuMin input file")
-  <*> switch ( long "with-prelude" <> help "whether to include prelude functions in output")
+  <*> strOption (short 'o' <> metavar "OUTPUT" <> value "Out.cumin" <> help "the SaLT output file")
+  <*> switch (long "with-prelude" <> help "whether to include prelude functions in output")
 
 act :: Options -> IO ()
-act (Options { inputFile, withPrelude }) =
+act (Options { inputFile, outputFile, withPrelude }) =
   CM.buildModuleFromFile inputFile >>= \case
     Left msg -> print msg
     Right modul -> case importUnqualified modul preludeModule of
       Left conflicts -> putStrLn "Conflicts with names in Prelude: " >> print conflicts
       Right modulWithPrelude ->
-        SP.displayPretty 80
-        . SP.prettyModule
-        . (if withPrelude then id else filterPrelude)
-        . cuminToSalt
-        $ modulWithPrelude
+        writeFile outputFile
+          . flip PP.displayS ""
+          . PP.renderPretty 0.7 80
+          . PP.plain
+          . SP.prettyModule
+          . (if withPrelude then id else filterPrelude)
+          . cuminToSalt
+          $ modulWithPrelude
