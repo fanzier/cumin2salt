27102be146e3e44141e2a2c76cf7979e59894d7f
Fabian Zaiser <fabian.zaiser@googlemail.com>
2015-03-02 18:09:36 +0100

    Type check input file before translation.

--- cumin2salt/Main.hs
+++ cumin2salt/Main.hs
@@ -2,9 +2,13 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
+import           Data.Default.Class
+import qualified Data.Map                         as M
 import           FunLogic.Core.ModBuilder         (importUnqualified)
+import qualified Language.CuMin.AST               as C
 import qualified Language.CuMin.ModBuilder        as CM
 import           Language.CuMin.Prelude           (preludeModule)
+import qualified Language.CuMin.TypeChecker       as CT
 import           Language.CuminToSalt.Translation (cuminToSalt, filterPrelude)
 import qualified Language.SaLT.Pretty             as SP
 import           Options.Applicative
@@ -32,16 +36,28 @@ optionParser = Options
 
 act :: Options -> IO ()
 act (Options { inputFile, outputFile, withPrelude }) =
-  CM.buildModuleFromFile inputFile >>= \case
-    Left msg -> print msg
+  checkFile inputFile >>= \case
+    Nothing -> putStrLn $ "\nLoading `" ++ inputFile ++ "` failed."
+    Just modulWithPrelude ->
+      writeFile outputFile
+      . flip PP.displayS ""
+      . PP.renderPretty 0.7 80
+      . PP.plain
+      . SP.prettyModule
+      . (if withPrelude then id else filterPrelude)
+      . cuminToSalt
+      $ modulWithPrelude
+
+checkFile :: FilePath -> IO (Maybe C.Module)
+checkFile cuminFile =
+  CM.buildModuleFromFile cuminFile >>= \case
+    Left msg -> PP.putDoc (PP.pretty msg) >> return Nothing
     Right modul -> case importUnqualified modul preludeModule of
-      Left conflicts -> putStrLn "Conflicts with names in Prelude: " >> print conflicts
-      Right modulWithPrelude ->
-        writeFile outputFile
-          . flip PP.displayS ""
-          . PP.renderPretty 0.7 80
-          . PP.plain
-          . SP.prettyModule
-          . (if withPrelude then id else filterPrelude)
-          . cuminToSalt
-          $ modulWithPrelude
+      Left (adtConflicts, functionConflicts) -> do
+        putStrLn "Some names in the module conflict with prelude names:"
+        mapM_ (putStrLn . fst) (M.toList adtConflicts)
+        mapM_ (putStrLn . fst) (M.toList functionConflicts)
+        return Nothing
+      Right modulWithPrelude -> case CT.evalTC (CT.checkModule modulWithPrelude) def def of
+        Left msg -> PP.putDoc (PP.pretty msg) >> return Nothing
+        Right () -> return $ Just modulWithPrelude
